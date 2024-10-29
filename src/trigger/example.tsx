29af12a95f44520b522d10055219b80bfba181d3
ShouvikGhosh2048 <shouvikg.cal@gmail.com>
2024-10-30 00:16:28 +0530

    Add new repos automatically

--- src/trigger/example.tsx
+++ src/trigger/example.tsx
@@ -6,6 +6,7 @@ import { Octokit } from "octokit";
 import { Resend } from "resend";
 import ReviewReminder from "../../emails/reviewReminder";
 import * as React from "react";
+import { eq } from "drizzle-orm";
 
 const octokit = new Octokit({ auth: process.env.OCTOKIT_TOKEN });
 const conn = postgres(process.env.DATABASE_URL!);
@@ -78,5 +79,92 @@ export const checkRepos = schedules.task({
     for (const email of emails) {
       await resend.emails.send(email);
     }
+  },
+});
+
+export const addNewRepos = schedules.task({
+  id: "add-new-repos",
+  cron: "0 6 * * *",
+  run: async (payload) => {
+    const [projects, githubUsers] = await Promise.all([
+      db.query.projects.findMany({
+        columns: {
+          repository: true,
+        },
+        with: {
+          user: {
+            columns: {
+              id: true,
+            }
+          }
+        }
+      }),
+      db.query.users.findMany({
+        columns: {
+          id: true,
+          
+        },
+        with: {
+          accounts: {
+            columns: {
+              providerAccountId: true,
+            },
+            // TODO: Can there be multiple Github accounts for the same user?
+            where: eq(schema.accounts.provider, "github"),
+          }
+        }
+      })
+    ]);
+
+    const userIdToProjects = new Map<string, Set<string>>();
+    projects.forEach(project => {
+      const projects = userIdToProjects.get(project.user.id);
+      if (projects) {
+        projects.add(project.repository);
+      } else {
+        userIdToProjects.set(project.user.id, new Set([project.repository]));
+      }
+    });
+
+    const newProjects: { userId: string, repository: string }[] = [];
+    await Promise.all(githubUsers.map(async (user) => {
+      try {
+        // TODO: I assume 1 Github account per user here.
+        const projects = userIdToProjects.get(user.id) ?? new Set<string>();
+
+        if (!user.accounts[0]) {
+          return;
+        }
+        const userDetails = await octokit.request('GET /user/{account_id}', {
+          account_id: user.accounts[0].providerAccountId,
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+          }
+        });
+        const githubRepos = ((await octokit.request('GET /users/{username}/repos', {
+          username: userDetails.data.login,
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+          },
+          sort: 'created',
+        })).data as { full_name: string, created_at: string }[]).map(repo => ({ name: repo.full_name, createdAt: new Date(repo.created_at) }));
+        // TODO: Technically you should fetch more repos, since this is only the first page.
+        // TODO: Maybe check that we don't add repos which were created before the user joined.
+
+        const MILLISECONDS_IN_A_DAY = 24 * 60 * 60 * 1000;
+        githubRepos.forEach(repo => {
+          const created = Math.floor(repo.createdAt.valueOf() / MILLISECONDS_IN_A_DAY);
+          const today = Math.floor(payload.timestamp.valueOf() / MILLISECONDS_IN_A_DAY);
+          if (today - created <= 1 && !projects.has(repo.name)) {
+            newProjects.push({
+              userId: user.id,
+              repository: repo.name,
+            });
+          }
+        });
+      } catch (err) {}
+    }));
+
+    await db.insert(schema.projects).values(newProjects).onConflictDoNothing();
   },
 });
